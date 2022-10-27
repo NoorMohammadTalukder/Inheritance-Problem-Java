5e73e1e7598dea579490c7ac1a86121f27b2b066
YourName <noor11809027@gmail.com>
2022-10-27 18:38:17 +0600

    done

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+    
+    Manager m1=new Manager(1,"Noor",2000,"Dhaka");
+    m1.showManagerDetails();
+
+    Salesman s1=new Salesman(2,"Abdullah",500,"Dhaka","T-Shirt",1);
+    s1.showSalesmanDetails();
+
+    System.out.println("-------------After Increament-----------------");
+    m1.incrementSalary(s1,1000);
+    s1.showSalesmanDetails();
+   
+  }
+}
